36dbebd36f01315f175ae8d9b05f40bf9fa13308
Daniel Gómez <danigb92trig@gmail.com>
2021-02-26 14:17:39 +0100

    4 Terminamos de refactorizar el código

--- Tarea3.java
+++ Tarea3.java
@@ -17,31 +17,15 @@ public class Tarea3 {
     public static void main(String[] args) {
 
         int[] miArray1 = new int[10];
-        int i, j, n = miArray1.length, aux = 0;
-        for (i = 0; i < miArray1.length; i++) {
-            miArray1[i] = (int) (Math.random() * 50);
-        }
-        for (i = 0; i < n - 1; i++) {
-            for (j = i + 1; j < n; j++) {
-                if (miArray1[i] > miArray1[j]) {
-                    aux = miArray1[j];
-                    miArray1[j] = miArray1[i];
-                    miArray1[i] = aux;
-                }
-            }
-        }
-        float temp0;
-        float temp1;
-        int pos;
+
+        rellenaArray(miArray1);
+        organizarPendientes(miArray1);
+
         if (miArray1.length % 2 == 0) {
-            pos = (miArray1.length / 2) + 1;
-            temp0 = miArray1[pos + 1];
-            System.out.println("temp: " + temp0);
+            System.out.println("temp: " + miArray1[miArray1.length / 2 + 2]);
         }
-        if (n % 2 == 1) {
-            pos = miArray1.length / 2;
-            temp1 = (miArray1[pos]);
-            System.out.println("temp: " + temp1);
+        if (miArray1.length % 2 == 1) {
+            System.out.println("temp: " + miArray1[miArray1.length / 2]);
         }
     }
 
