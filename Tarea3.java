e32f07a64bb3458288c48559c57ba70de755745b
Daniel Gómez <danigb92trig@gmail.com>
2021-02-26 14:01:27 +0100

    1 - Creamos el proyecto Tarea3

---
+++ Tarea3.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tarea3;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Tarea3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int[] miArray1 = new int[10];
+        int i, j, n = miArray1.length, aux = 0;
+        for (i = 0; i < miArray1.length; i++) {
+            miArray1[i] = (int) (Math.random() * 50);
+        }
+        for (i = 0; i < n - 1; i++) {
+            for (j = i + 1; j < n; j++) {
+                if (miArray1[i] > miArray1[j]) {
+                    aux = miArray1[j];
+                    miArray1[j] = miArray1[i];
+                    miArray1[i] = aux;
+                }
+            }
+        }
+        float temp0;
+        float temp1;
+        int pos;
+        if (miArray1.length % 2 == 0) {
+            pos = (miArray1.length / 2) + 1;
+            temp0 = miArray1[pos + 1];
+            System.out.println("temp: " + temp0);
+        }
+        if (n%2==1){
+            pos = miArray1.length/2;
+            temp1 = (miArray1[pos]);
+            System.out.println("temp: "+ temp1);
+        }
+    }
+
+}
